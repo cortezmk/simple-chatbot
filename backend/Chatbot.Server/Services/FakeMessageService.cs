4a659f74d5ffe84623eb7ddbb1ecf2cbd35afbb7
cortezmk <cortez.mk@gmail.com>
2025-02-26 14:53:47 +0100

    initial

---
+++ backend/Chatbot.Server/Services/FakeMessageService.cs
@@ -0,0 +1,65 @@
+﻿using Chatbot.Server.Models;
+using Microsoft.EntityFrameworkCore;
+using NLipsum.Core;
+using System.Linq;
+
+
+namespace Chatbot.Server.Services;
+
+public class FakeMessageService(MessageContext DbContext) : IMessageService
+{
+    private readonly MessageContext _dbContext = DbContext;
+
+    private readonly LipsumGenerator _lorem = new();
+
+    public async Task<Message> AddMessage(string content, MessageAuthor author)
+    {
+        var newMessage = new Message(0, content, DateTime.Now, author, string.Empty);
+        _dbContext.Messages.Add(newMessage);
+        await _dbContext.SaveChangesAsync();
+        return newMessage;
+    }
+
+    public async Task<List<Message>> GetAllMessages()
+    {
+        return await _dbContext.Messages.ToListAsync();
+    }
+
+    public async IAsyncEnumerable<string> GetResponse(string message)
+    {
+        var length = Random.Shared.Next(3);
+        var generated = _lorem.GenerateLipsum(length);
+        for (var i = 0; i < generated.Length; i += 3)
+        {
+            i = Math.Min(i, generated.Length);
+            yield return generated.Substring(0, i + 1);
+            await Task.Delay(20);
+        }
+        yield return generated;
+    }
+
+    public async Task<Message> UpdateLast(string content)
+    {
+        var found = await _dbContext.Messages.AsNoTracking().OrderBy(m => m.Id).LastOrDefaultAsync();
+        if (found == null)
+            return null;
+        found = found with { Content = content };
+        _dbContext.Messages.Update(found);
+        await _dbContext.SaveChangesAsync();
+        return found;
+    }
+
+    public async Task<Message> UpdateMessage(Message message)
+    {
+        var found = await _dbContext.Messages.AsNoTracking().FirstOrDefaultAsync(m => m.Id == message.Id);
+        if (found == null)
+        {
+            var newMessage = await _dbContext.Messages.AddAsync(message);
+            await _dbContext.SaveChangesAsync();
+            return newMessage.Entity;
+        }
+        _dbContext.Messages.Update(message);
+        await _dbContext.SaveChangesAsync();
+        return message;
+    }
+}
---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,113 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { Observable } from 'rxjs';
+import { Message } from './message';
+import { MessagesService } from './messages.service';
+import { AsyncPipe } from '@angular/common';
+import { MessageComponent } from './message/message.component';
+import {MatSlideToggleChange, MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { CdkTextareaAutosize } from '@angular/cdk/text-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import { ISubscription } from '@microsoft/signalr';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    RouterOutlet,
+    AsyncPipe,
+    MatToolbarModule,
+    MatIconModule,
+    MessageComponent,
+    MatSlideToggleModule,
+    CdkTextareaAutosize,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatButtonModule,
+    MatProgressSpinnerModule
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'frontend';
+
+  constructor(private messagesService: MessagesService,
+
+  ) {
+    this.messages$ = messagesService.getMessages();
+  }
+
+  @ViewChild('main') mainElement!: ElementRef;
+
+  messages$: Observable<Message[]>;
+  newMessage: string = '';
+  newMessages: Message[] = [];
+  currentAssistantMessage: Message | null = null;
+  isStreaming = false;
+
+  private stream: ISubscription<string> | null = null;
+
+  async sendMessage() {
+    console.log('sendMessage');
+    const newMessage = this.newMessage;
+    this.newMessage = '';
+    let addedMessage = await this.messagesService.addMessage(newMessage, 'User');
+    this.newMessages.push(addedMessage);
+    addedMessage = await this.messagesService.addMessage('', 'Assistant');
+    this.newMessages.push(addedMessage);
+    this.currentAssistantMessage = addedMessage;
+    await this.handleStream(newMessage);
+  }
+
+  async updateMessage(message: Message) {
+    await this.messagesService.updateMessage(message);
+  }
+
+  onModeChanged(arg: MatSlideToggleChange) {
+    const element = document.getElementsByTagName('html')[0];
+    element.classList.remove(arg.checked ? 'light' : 'dark');
+    element.classList.add(arg.checked ? 'dark' : 'light');
+  }
+
+  async handleStream(message: string) {
+    console.log('handle stream');
+    const stream = await this.messagesService.receiveResponse(message);
+    this.isStreaming = true;
+    this.stream = stream.subscribe({
+      next: (m: string) => {
+        console.log('next');
+        if(this.currentAssistantMessage)
+          this.currentAssistantMessage.content = m;
+      },
+      error: (error) => {
+        console.log(error);
+        this.isStreaming = false;
+      },
+      complete: () => this.isStreaming = false
+    })
+  }
+
+  cancelMessage() {
+    this.messagesService.cancelMessage();
+  }
+
+  ngAfterViewInit() {
+    this.mainElement.nativeElement.scrollTop = 
+      this.mainElement.nativeElement.scrollHeight;
+    // this.mainElement.nativeElement
+    //   .scrollTo(0, this.mainElement.nativeElement.scrollHeight);
+  }
+
+  ngOnDestroy() {
+    if(this.stream)
+      this.stream.dispose();
+    this.messagesService.closeConnection();
+  }
+}
